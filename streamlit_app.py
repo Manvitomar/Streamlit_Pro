3ce1d206d4900ca09145c0c9391b24588bddaa79
Manvitomar <manvitomar046@gmail.com>
2026-02-06 19:21:35 +0530

    creatingNewPro

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,17 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-st.header("st.slider")
-age=st.slider(
-    "Enter you age",0,100
-)
-st.write("Your age is ",age)
 
-line_chart=pd.DataFrame(np.random.rand(20,3),columns=['a','b','c'])
-st.line_chart(line_chart)
+st.title('Customizing the theme of Streamlit apps')
 
-st.write("st.selectbox")
-option=st.selectbox("What is your favourite color",('red','blue','green'))
-st.write("Your favourite color is",option)
-         
-st.write("st.multiselect")
-options = st.multiselect(
-     'What are your favorite colors',
-     ['Green', 'Yellow', 'Red', 'Blue'])
+st.write('Contents of the `.streamlit/config.toml` file of this app')
 
-st.write('You selected:')
-for option in options:
-  st.write(option)
+st.code("""
+[theme]
+primaryColor="#FFFFFF"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+""")
 
-
-  st.write(" Creating check box ")
-
-  tea=st.checkbox("Tea")
-  coffee=st.checkbox("Coffee")
-  icecream=st.checkbox("iceCream")
-
-  if tea:
-    st.write("Here is your tea")
-  if coffee:
-   st.write("Here is your coffee")
-  if icecream:
-    st.write("Here is your icecream")    
+number = st.slider('Select a number:', 0, 90)
+st.sidebar.write('Selected number from slider widget is:', number)
